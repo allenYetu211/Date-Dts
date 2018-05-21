835edea73b248f011ffe1b440f8d5499136c3f60
allen <allenwill211@gmail.com>
2018-05-22 00:16:08 +0800

    change: constructor

--- src/TDate/timeCycle.js
+++ src/TDate/timeCycle.js
@@ -8,18 +8,19 @@ let timeType = {
 }
 
 const dtsMoment = (...theArg) => {
-    return disposeConfig.apply(null,theArg)
+     disposeConfig.apply(null,theArg)
 }
 
-const disposeConfig = (...theArg) => {
-    if (theArg.length === 0 ) {
-        return createLocalTime(new window.Date())
-    } else if (theArg.length === 1) {
-        return verificationTime(theArg[0])
+const disposeConfig = (date, type) => {
+    let time = new Date()
+    if (date !== undefined && Object.time != 'Invalid Date') {
+        throw 'please input correct param!'
     }
+    let config = createLocalTime(time,date, type)
+    createInstance(config)
 }
 
-const createLocalTime = (time) =>  {
+const createLocalTime = (time, date, type) =>  {
     let Date = time
     let dts = {
         _y:  Date.getFullYear(),                                  // 年
@@ -29,10 +30,33 @@ const createLocalTime = (time) =>  {
         _h:  disposeTime(Date.getHours()),                        // 时
         _mt: disposeTime(Date.getMinutes()),                      // 分
         _se: disposeTime(Date.getSeconds()),                      // 秒
-        _tp: Date.getTime()                                       // 时间戳
+        _tp: Date.getTime(),                                      // 时间戳
+        _input: date,
+        _type : type
     }
     return dts
 }
+
+const createInstance = (config) => {
+    let res = new TimeCycle(config)
+    window.res = res
+    return res
+}
+
+// const createLocalTime = (time) =>  {
+//     let Date = time
+//     let dts = {
+//         _y:  Date.getFullYear(),                                  // 年
+//         _m:  disposeTime(Date.getMonth() + 1),                    // 月
+//         _d:  disposeTime(Date.getDate()),                         // 日期
+//         _dy:  Date.getDay() === 0 ? 7 : Date.getDay(),            // 星期
+//         _h:  disposeTime(Date.getHours()),                        // 时
+//         _mt: disposeTime(Date.getMinutes()),                      // 分
+//         _se: disposeTime(Date.getSeconds()),                      // 秒
+//         _tp: Date.getTime()                                       // 时间戳
+//     }
+//     return dts
+// }
 const disposeTime = (param) => {
     return param < 10 ? '0' + param : param
 }
@@ -48,26 +72,34 @@ const verificationTime = (date) => {
     return dts
 }
 
-
-const verifyConfig = (to , from ) =>{
-   for (let i in from) {
-       if (from[i] !== undefined) {
-           to[i] = from[i]
-       }
-   }
-   return to
- }
-
+const copyConfig = (to, from) => {
+    Object.keys(from).map(e => {
+        if (from[e] !== undefined) {
+            to[e] = from[e]
+        }
+    })
+}
 
 class TimeCycle {
     constructor(config) {
-        // verifyConfig(this, config)
-        this._d = new Date(
-            config._d != null ? config._d.getTime() : NaN
-        )
+        copyConfig(this, config)
+        this.verificationConfig(config)
+
     }
 
+    verificationConfig(config) {
+        if (!!config._type)   {
+            timeType[config._type]
+        }
+    }
+
+    getTime() {
+
+    }
 
 }
+
+
+
 
 export default dtsMoment
